a3d862c8e4408bda591f2610baf8835bcdf54129
Asafe Orneles <asafeorneles@gmail.com>
2026-03-31 22:13:51 -0300

    refactor: Refatorei o meu migrations V1 para criar as tabelas das novas entidades. Não criei um V2 para manter a criação das tabelas primárias na V1.

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -7,14 +7,42 @@ CREATE TABLE tb_rooms
     column_quantity INT          NOT NULL
 );
 
+-- Tabela de filmes
+CREATE TABLE tb_movies
+(
+    movie_id     BINARY(16)   PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    duration    INT          NOT NULL,
+    movie_genre VARCHAR(50)  NOT NULL,
+    age_rating  VARCHAR(10)  NOT NULL
+);
+
+-- Tabela de sessões
+CREATE TABLE tb_sessions
+(
+    session_id     BINARY(16) PRIMARY KEY,
+    movie_id       BINARY(16) NOT NULL,
+    room_id        BINARY(16) NOT NULL,
+    start_time     DATETIME    NOT NULL,
+    end_time       DATETIME    NOT NULL,
+    session_type   VARCHAR(10) NOT NULL,
+    session_format VARCHAR(10) NOT NULL,
+
+    CONSTRAINT fk_session_movie
+        FOREIGN KEY (movie_id) REFERENCES tb_movies (movie_id),
+
+    CONSTRAINT fk_session_room
+        FOREIGN KEY (room_id) REFERENCES tb_rooms (room_id)
+);
+
 -- Tabela de assentos
 CREATE TABLE tb_seats
 (
-    seat_id       BINARY(16) PRIMARY KEY,
-    seat_name     VARCHAR(255) NOT NULL,
+    seat_id     BINARY(16) PRIMARY KEY,
+    seat_name   VARCHAR(255) NOT NULL,
     seat_row    INT          NOT NULL,
     seat_column INT          NOT NULL,
-    room_id       BINARY(16) NOT NULL,
+    room_id     BINARY(16) NOT NULL,
 
     CONSTRAINT fk_seat_room
         FOREIGN KEY (room_id) REFERENCES tb_rooms (room_id),
@@ -27,9 +55,14 @@ CREATE TABLE tb_seats
 CREATE TABLE tb_reservations
 (
     reservation_id   BINARY(16) PRIMARY KEY,
+    session_id       BINARY(16) NOT NULL,
     status           VARCHAR(50) NOT NULL,
     reservation_date DATETIME    NOT NULL,
-    expiration_time  DATETIME    NOT NULL
+    expiration_time  DATETIME    NOT NULL,
+
+    CONSTRAINT fk_reservation_session
+        FOREIGN KEY (session_id) REFERENCES tb_sessions (session_id)
+
 );
 
 -- Tabela intermediária (SeatReservation)
